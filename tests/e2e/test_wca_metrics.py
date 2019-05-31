9c22ec6af5e03532d5fdecc80300e4c82952c96b
Marta Mucek <marta.mucek@intel.com>
2019-05-31 16:50:50 +0200

    WIP

---
+++ tests/e2e/test_wca_metrics.py
@@ -0,0 +1,88 @@
+# Copyright (C) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from requests import get
+from time import time
+
+import logging
+import os
+import pytest
+
+_PROMETHEUS_QUERY_PATH = "/api/v1/query"
+_PROMETHEUS_QUERY_RANGE_PATH = "/api/v1/query_range"
+_PROMETHEUS_URL_TPL = '{prometheus}{path}?query={name}'
+_PROMETHEUS_TIME_TPL = '&start={start}&end={end}&step=1s'
+_PROMETHEUS_TAG_TPL = '{key}="{value}"'
+
+
+def _build_prometheus_url(prometheus, name, tags=None, window_size=None, event_time=None):
+    tags = tags or dict()
+    path = _PROMETHEUS_QUERY_PATH
+    time_range = ''
+
+    # Some variables need to be overwritten for range queries.
+    if window_size and event_time:
+        offset = window_size / 2
+        time_range = _PROMETHEUS_TIME_TPL.format(
+            start=event_time - offset,
+            end=event_time + offset)
+        path = _PROMETHEUS_QUERY_RANGE_PATH
+
+    url = _PROMETHEUS_URL_TPL.format(
+        prometheus=prometheus,
+        path=path,
+        name=name,
+    )
+
+    # Prepare additional tags for query.
+    query_tags = []
+    for k, v in tags.items():
+        query_tags.append(_PROMETHEUS_TAG_TPL.format(key=k, value=v))
+    query_tags_str = ','.join(query_tags)
+
+    # Build final URL from all the components.
+    url = ''.join([url, "{", query_tags_str, "}", time_range])
+
+    return url
+
+
+def _fetch_metrics(url):
+    response = get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+@pytest.mark.parametrize('workload_instance', [])
+def test_wca_metrics(workload_instance):
+    assert 'PROMETHEUS' in os.environ, 'prometheus host to connect'
+    assert 'BUILD_NUMBER' in os.environ
+    assert 'BUILD_COMMIT' in os.environ
+
+    prometheus = os.environ['PROMETHEUS']
+    build_number = int(os.environ['BUILD_NUMBER'])
+    build_commit = os.environ['BUILD_COMMIT']
+
+    tags = dict(build_number=build_number,
+                build_commit=build_commit,
+                workload_instance=workload_instance)
+
+    logging.info('build number = %r', build_number)
+    prometheus_query = _build_prometheus_url(prometheus,
+                                             tags, 1800, time())
+    metrics = _fetch_metrics(prometheus_query)
+    assert len(metrics) > 0
+
