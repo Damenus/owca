35e299e264c94836d5ac554b2270cae6426e2083
Marta Mucek <marta.mucek@intel.com>
2019-06-07 14:49:52 +0200

    WIP

--- tests/e2e/test_wca_metrics.py
+++ tests/e2e/test_wca_metrics.py
@@ -73,7 +73,9 @@ def _fetch_metrics(url):
     ('cassandra_ycsb', '34'),
     ('specjbb', '34'),
     ('stress_ng', '34'),
-    ('twemcache_rpc_perf', '34')
+    ('twemcache_rpc_perf', '34'),
+    ('tensorflow_benchmark_prediction', '34'),
+    ('tensorflow_benchmark_train', '34')
 ])
 def test_wca_metrics_kubernetes(workload_name, env_uniq_id):
     assert 'PROMETHEUS' in os.environ, 'prometheus host to connect'
