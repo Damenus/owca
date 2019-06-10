73e6f9d3f2db5388e0c4e6530bc7fe8f6fcaec40
Damian Darczuk <damian.darczuk@intel.com>
2019-06-10 13:14:15 +0200

    Fix

--- wca/cgroups.py
+++ wca/cgroups.py
@@ -83,16 +83,12 @@ class Cgroup:
                 CgroupSubsystem.PERF_EVENT, relative_cgroup_path)
 
     def get_measurements(self) -> Measurements:
-        with open(os.path.join(self.cgroup_cpu_fullpath, CgroupResource.CPU_USAGE)) as \
-                cpu_usage_file:
-            cpu_usage = int(cpu_usage_file.read())
         try:
-            with open(os.path.join(self.cgroup_fullpath, CPU_USAGE)) as \
+            with open(os.path.join(self.cgroup_cpu_fullpath, CgroupResource.CPU_USAGE)) as \
                     cpu_usage_file:
                 cpu_usage = int(cpu_usage_file.read())
         except FileNotFoundError:
             log.warning('Could not read measurements for container %s. ', self._cgroup_path)
-            # Returning empty measurements.
             return {}
 
         return {MetricName.CPU_USAGE_PER_TASK: cpu_usage}
