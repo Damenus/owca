4106f8fdfe3851fad2713af03228178b927d7b55
Marta Mucek <marta.mucek@intel.com>
2019-06-17 14:30:45 +0200

    WIP

--- tests/e2e/test_wca_metrics.py
+++ tests/e2e/test_wca_metrics.py
@@ -92,15 +92,15 @@ def test_wca_metrics_kubernetes(workload_name, env_uniq_id):
                 env_uniq_id=env_uniq_id)
 
     logging.info('build number = %r', build_number)
-    sli_query = _build_prometheus_url(prometheus, 'sli',
+    slo_query = _build_prometheus_url(prometheus, 'slo',
                                       tags, 1800, time())
-    sli_metrics = _fetch_metrics(sli_query)
-    assert len(sli_metrics['data']['result']) > 0
+    slo_metrics = _fetch_metrics(slo_query)
+    assert len(slo_metrics['data']['result']) > 0
 
-    instructions_query = _build_prometheus_url(prometheus, 'instructions',
+    cycles_query = _build_prometheus_url(prometheus, 'cycles',
                                                tags, 1800, time())
-    instructions_metrics = _fetch_metrics(instructions_query)
-    assert len(instructions_metrics['data']['result']) > 0
+    cycles_metrics = _fetch_metrics(cycles_query)
+    assert len(cycles_metrics['data']['result']) > 0
 
 
 @pytest.mark.parametrize('workload_name, env_uniq_id', [
@@ -127,12 +127,12 @@ def test_wca_metrics_mesos(workload_name, env_uniq_id):
                 env_uniq_id=env_uniq_id)
 
     logging.info('build number = %r', build_number)
-    sli_query = _build_prometheus_url(prometheus, 'sli',
+    slo_query = _build_prometheus_url(prometheus, 'slo',
                                       tags, 1800, time())
-    sli_metrics = _fetch_metrics(sli_query)
-    assert len(sli_metrics['data']['result']) > 0
+    slo_metrics = _fetch_metrics(slo_query)
+    assert len(slo_metrics['data']['result']) > 0
 
-    instructions_query = _build_prometheus_url(prometheus, 'instructions',
+    cycles_query = _build_prometheus_url(prometheus, 'cycles',
                                                tags, 1800, time())
-    instructions_metrics = _fetch_metrics(instructions_query)
-    assert len(instructions_metrics['data']['result']) > 0
+    cycles_metrics = _fetch_metrics(cycles_query)
+    assert len(cycles_metrics['data']['result']) > 0
