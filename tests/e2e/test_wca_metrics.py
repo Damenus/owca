344a7fc91b62d64a44d52e6a8e48cef1495b53e8
Damian Darczuk <damian.darczuk@intel.com>
2020-01-17 15:28:29 +0100

    E2E, hammerdb to hammerdb-small

--- tests/e2e/test_wca_metrics.py
+++ tests/e2e/test_wca_metrics.py
@@ -88,7 +88,7 @@ def test_wca_metrics_kustomize(workload_name):
 
 
 @pytest.mark.parametrize('workload_name', [
-    'hammerdb'
+    'hammerdb-small'
 ])
 def test_wca_metrics_kustomize_throughput(workload_name):
     test_wca(workload_name, ['apm__sli2', 'task_cycles'])
