df24b8eed532cebbc68332986b3908d19d05a43a
Damian Darczuk <damian.darczuk@intel.com>
2020-01-28 13:36:47 +0100

    Seting parameters

--- examples/kubernetes/workloads/mysql-hammerdb/base/build_workload.tcl
+++ examples/kubernetes/workloads/mysql-hammerdb/base/build_workload.tcl
@@ -11,9 +11,9 @@ diset connection mysql_host MYSQL_HOST
 diset connection mysql_port 3306
 diset tpcc mysql_user testuser
 diset tpcc mysql_pass testpassword
-diset tpcc mysql_count_ware 4
+diset tpcc mysql_count_ware COUNT_WARE
 diset tpcc mysql_partition true
-diset tpcc mysql_num_vu 4
+diset tpcc mysql_num_vu WORKER_BUILDING_SCHEMA
 diset tpcc mysql_storage_engine innodb
 print dict
 buildschema
